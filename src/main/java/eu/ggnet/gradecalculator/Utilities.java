e79cab35981da9b8a9ecc0803503f172ebaf0da9
Mirko Schulze <avatarmkvii@hotmail.com>
2019-07-24 18:09:15 +0200

    Update Utilities.java
    alertWarn() + alertError()

--- src/main/java/eu/ggnet/gradecalculator/Utilities.java
+++ src/main/java/eu/ggnet/gradecalculator/Utilities.java
@@ -2,10 +2,6 @@ package eu.ggnet.gradecalculator;
 
 import eu.ggnet.gradecalculator.model.Grade.Mark;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 /**
  * Static class with useful methods for multiple situations.
@@ -29,12 +25,16 @@ public class Utilities {
     }
 
     /**
-     * Displays a new {@link Stage} with {@link Label} and {@link Button}
-     * components to indicate failures.
+     * Displays a new {@link Alert} with a message to indicate failures.
+     *
+     * @param message String to give detailed information about the error.
      */
     public static void alertWarn(String message) {
-        new Alert(Alert.AlertType.WARNING, "Something went wrong.\n"
-                + "But don't worry. It's your fault, probably. :)\n\n" + message, ButtonType.CLOSE).show();
+        new Alert(Alert.AlertType.WARNING, message).show();
+    }
+    
+    public static void alertError(String message){
+        new Alert(Alert.AlertType.ERROR, message).show();
     }
 
 }
