5652a50a6e182e62bf264753510f954311d05880
Mirko Schulze <avatarmkvii@hotmail.com>
2019-07-28 15:21:45 +0200

    ui.create package now with Dialog panes

--- src/main/java/eu/ggnet/gradecalculator/ui/create/CreateClassbookDialog.java
+++ src/main/java/eu/ggnet/gradecalculator/ui/create/CreateClassbookDialog.java
@@ -2,40 +2,53 @@ package eu.ggnet.gradecalculator.ui.create;
 
 import eu.ggnet.gradecalculator.model.Classbook;
 import java.util.ArrayList;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 /**
- * Displays a {@link Stage} to add a new {@link Classbook}.
+ * Invokes a specified Dialog pane with a {@link Label} and a {@link TextField}
+ * to create a new Classbook.
+ * <p>
+ * The created Classbook is the return value of the constructor, if a title is
+ * entered and the finish button is clicked, else the return value is null.
+ * <p>
+ * This can be used by calling the {@link Dialog#showAndWait() } method.
  *
  * @author Mirko Schulze
  */
 public class CreateClassbookDialog extends Dialog<Classbook> {
 
     public CreateClassbookDialog() {
+        this.setTitle("Klasse hinzfügen");
+        this.setHeaderText("Tragen Sie einen Namen für die Schulklasse ein.");
         TextField titleInput = new TextField();
+        titleInput.setPromptText("IT 8i");
         titleInput.setTooltip(new Tooltip("Tragen Sie einen Namen wie z.B. \"IT 8i\" ein."));
 
         HBox hbox = new HBox(5, new Label("Klassenname: "), titleInput);
         hbox.setAlignment(Pos.CENTER);
         hbox.setPadding(new Insets(5));
+
+        this.getDialogPane().setContent(hbox);
         this.getDialogPane().getButtonTypes().addAll(ButtonType.FINISH, ButtonType.CANCEL);
-        this.setResultConverter(type -> {
-            if (type == ButtonType.FINISH) {
-                return new Classbook(titleInput.getText(), new ArrayList<>());
-            } else {
-                return null;
+        Button finishButton = (Button) this.getDialogPane().lookupButton(ButtonType.FINISH);
+        finishButton.addEventFilter(ActionEvent.ACTION, eh -> {
+            if (titleInput.getText().isEmpty()) {
+                eh.consume();
             }
         });
-        this.setHeaderText("Tragen Sie einen Namen für die Schulklasse ein.");
-        this.getDialogPane().setContent(hbox);
+        this.setResultConverter(type -> {
+            return (type == ButtonType.FINISH) ? new Classbook(titleInput.getText(), new ArrayList<>()) : null;
+        });
+
     }
 
 }
--- src/main/java/eu/ggnet/gradecalculator/ui/create/CreatePupilDialog.java
+++ src/main/java/eu/ggnet/gradecalculator/ui/create/CreatePupilDialog.java
@@ -1,56 +1,104 @@
 package eu.ggnet.gradecalculator.ui.create;
 
-import eu.ggnet.gradecalculator.model.Classbook;
+import eu.ggnet.gradecalculator.Utilities;
 import eu.ggnet.gradecalculator.model.Pupil;
 import eu.ggnet.gradecalculator.model.Pupil.Sex;
 import java.util.Arrays;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
 /**
- * Displays a {@link Stage} to add a new {link Pupil} to a {@link Classbook}.
+ * Invokes a specified Dialog pane with {@link Label} and {@link TextField}
+ * components to create a new Pupil.
+ * <p>
+ * The created Pupil is the return value of the constructor, if valid values are
+ * entered and the finish button is clicked, else the return value is null.
+ * <p>
+ * This can be used by calling the {@link Dialog#showAndWait() } method.
  *
  * @author Mirko Schulze
  */
 public class CreatePupilDialog extends Dialog<Pupil> {
 
+    String forename, surname;
+    int age;
+    TextField forenameInput, surnameInput, ageInput;
+    ComboBox<Sex> selectSexBox;
+
     public CreatePupilDialog() {
-        TextField forenameInput = new TextField("Max");
-        TextField surnameInput = new TextField("Mustermann");
-        TextField ageInput = new TextField("23");
-        ComboBox<Sex> selectSexBox = new ComboBox<>();
+        this.setTitle("Schüler hinzufügen");
+        this.setHeaderText("Tragen Sie für alle Felder Werte ein,\num einen neuen Schüler zu einer Klasse hinzuzufügen.");
+
+        forenameInput = new TextField();
+        forenameInput.setPromptText("Max");
+        forenameInput.setTooltip(new Tooltip("Trage einen Vornamen ein, wie z.b. \"Maria\""));
+
+        surnameInput = new TextField();
+        surnameInput.setPromptText("Mustermann");
+        surnameInput.setTooltip(new Tooltip("Trage einen Nachnamen ein, wie z.B. \"Musterfrau\""));
+
+        ageInput = new TextField();
+        ageInput.setPromptText("23");
+        ageInput.setTooltip(new Tooltip("Trage ein gültiges Alter in Jahren ein, wie z.B. \"100\""));
+
+        selectSexBox = new ComboBox<>();
         selectSexBox.setItems(FXCollections.observableArrayList(Arrays.asList(Sex.values())));
         selectSexBox.setValue(Sex.MALE);
+        selectSexBox.setTooltip(new Tooltip("Wähle ein Geschlecht aus"));
 
         GridPane grid = new GridPane();
+        grid.setHgap(5);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(5));
         grid.addColumn(0, new Label("Vorname: "));
         grid.addColumn(0, new Label("Nachname: "));
         grid.addColumn(0, new Label("Alter: "));
         grid.addColumn(0, new Label("Geschlecht: "));
-
         grid.addColumn(1, forenameInput);
         grid.addColumn(1, surnameInput);
         grid.addColumn(1, ageInput);
         grid.addColumn(1, selectSexBox);
 
-        grid.setHgap(5);
-        grid.setVgap(5);
-
         this.getDialogPane().getButtonTypes().addAll(ButtonType.FINISH, ButtonType.CANCEL);
+        Button finishButton = (Button) this.getDialogPane().lookupButton(ButtonType.FINISH);
+        finishButton.addEventFilter(ActionEvent.ACTION, eh -> {
+            if (this.forenameInput.getText().isEmpty()
+                    | this.surnameInput.getText().isEmpty()
+                    | this.ageInput.getText().isEmpty()) {
+                eh.consume();
+                Utilities.alertWarn("Bitte füllen Sie alle Felder korrekt aus.");
+            } else {
+                String name = this.forenameInput.getText().trim().toLowerCase().replaceAll("\\W|\\d", "");
+                this.forename = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+                name = this.surnameInput.getText().trim().toLowerCase().replaceAll("\\W|\\d", "");
+                this.surname = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+                try {
+                    this.age = Integer.parseInt(this.ageInput.getText().trim());
+                } catch (NumberFormatException e) {
+                    Utilities.alertWarn("Bitte geben Sie ein Alter in Ganzzahlen an.");
+                    eh.consume();
+                }
+            }
+        });
+
         this.setResultConverter(type -> {
             if (type == ButtonType.FINISH) {
-                return new Pupil(selectSexBox.getValue(), Integer.parseInt(ageInput.getText()), forenameInput.getText(), surnameInput.getText());
+                return new Pupil(this.selectSexBox.getSelectionModel().getSelectedItem(),
+                        this.age, this.forename, this.surname);
             } else {
                 return null;
             }
         });
-        this.setHeaderText("Tragen Sie für alle Felder Werte ein, um einen neuen Schüler zu einer Klasse hinzuzufügen.");
+
         this.getDialogPane().setContent(grid);
     }
 
